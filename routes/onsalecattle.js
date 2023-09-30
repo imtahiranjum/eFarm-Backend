a2394f6a1635da0e50a9bdbdc2c8f8049eaf3822
tahir_anjum <imtahiranjum@gmail.com>
2023-09-30 23:56:24 +0500

    "update cattle status" and "remove cattle"

--- routes/onsalecattle.js
+++ routes/onsalecattle.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { addCattleToSale, getOnSaleCattle, getOneOnSaleCattle } from "../controllers/onsalecattle.js";
+import { addCattleToSale, getAllOnSaleCattle, getOneOnSaleCattle, removeCattleFromSale, updateCattleOnSaleStatus } from "../controllers/onsalecattle.js";
 
 const router = express.Router();
 
-router.get("/", getOnSaleCattle);
+router.get("/", getAllOnSaleCattle);
 router.get("/specificonsalecattle", getOneOnSaleCattle);
 router.post("/addcattletosale", addCattleToSale);
+router.patch("/updatecattleonsalestatus/:cattle_id&:to_add", updateCattleOnSaleStatus);
+router.delete("/removefromsale/:id", removeCattleFromSale)
 
 export default router;
