a079ea95ef0da876ebb7abe10073046f298bc042
tahir_anjum <imtahiranjum@gmail.com>
2023-10-10 23:59:28 +0500

    added two routes for getting user

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { createUser, loginUser, logoutUser } from "../controllers/user.js";
+import { createUser, getUser, getUserId, loginUser, logoutUser } from "../controllers/user.js";
 
 const router = express.Router();
 
 router.post("/signup", createUser);
 router.post("/signin", loginUser);
+router.get("/", getUser);
+router.get("/id", getUserId);
 router.get("/logout", logoutUser);
 
 export default router;
