445c907e5eae6181147c9cae54d9520be5b42a6d
tahir_anjum <imtahiranjum@gmail.com>
2023-10-12 22:36:20 +0500

    updated and removed bugs from routes

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { createUser, getUser, getUserId, loginUser, logoutUser } from "../controllers/user.js";
+import { createUser, getUser, getUserByEmail, loginUser, logoutUser } from "../controllers/user.js";
 
 const router = express.Router();
 
 router.post("/signup", createUser);
 router.post("/signin", loginUser);
-router.get("/:id", getUser);
-router.get("/:email", getUserId);
-router.get("/logout", logoutUser);
+router.get("/id/:id", getUser);
+router.get("/email/:email", getUserByEmail);
+router.post("/logout", logoutUser);
 
 export default router;
