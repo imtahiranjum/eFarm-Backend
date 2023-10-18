cf532fd48729353c5bdf8d71101cea72cd3639a1
tahir_anjum <imtahiranjum@gmail.com>
2023-10-18 23:51:35 +0500

    added image getter route

--- routes/onsalecattle.js
+++ routes/onsalecattle.js
@@ -1,12 +1,24 @@
 import express from "express";
-import { addCattleToSale, getAllOnSaleCattle, getOneOnSaleCattle, removeCattleFromSale, updateCattleOnSaleStatus } from "../controllers/onsalecattle.js";
+import {
+  addCattleToSale,
+  getAllOnSaleCattle,
+  getOneOnSaleCattle,
+  getOneOnSaleCattleImages,
+  removeCattleFromSale,
+  updateCattleOnSaleStatus,
+} from "../controllers/onsalecattle.js";
+import multer from "multer";
 
 const router = express.Router();
 
 router.get("/", getAllOnSaleCattle);
 router.get("/specificonsalecattle/:id", getOneOnSaleCattle);
+router.get("/getonsalecattleimages/:id", getOneOnSaleCattleImages);
 router.post("/addcattletosale", addCattleToSale);
-router.patch("/updatecattleonsalestatus/:cattle_id&:to_add", updateCattleOnSaleStatus);
-router.delete("/removefromsale/:id", removeCattleFromSale)
+router.patch(
+  "/updatecattleonsalestatus/:cattle_id&:to_add",
+  updateCattleOnSaleStatus
+);
+router.delete("/removefromsale/:id", removeCattleFromSale);
 
 export default router;
