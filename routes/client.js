409036c4d5279e105171aa35ce6e37b23f834fd3
tahir_anjum <imtahiranjum@gmail.com>
2023-10-20 23:54:48 +0500

    updated client router

--- routes/client.js
+++ routes/client.js
@@ -1,7 +1,6 @@
 import express from "express";
 import {
     getStaff,
-    getDoctor,
     getEmployee,
     
 } from "../controllers/client.js";
@@ -9,7 +8,6 @@ const router = express.Router();
 
 
 router.get("/staff", getStaff);
-router.get("/doctors", getDoctor);
 router.get("/employee/:id", getEmployee);
 
 
