2a603bd91e576d93db9c9c446b853046a17a9b56
tahir_anjum <imtahiranjum@gmail.com>
2023-10-24 22:47:43 +0500

    added seller route

--- routes/general.js
+++ routes/general.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getDashboardStats } from "../controllers/general.js";
+import { getDashboardStats, getSeller } from "../controllers/general.js";
 
 
 const router = express.Router();
 
 router.get("/dashboard", getDashboardStats);
+router.get("/seller/:id", getSeller);
 
 export default router;
