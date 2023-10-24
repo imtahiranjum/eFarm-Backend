56cb29ba6894bf8df8bbf2f9d577999917816e66
tahir_anjum <imtahiranjum@gmail.com>
2023-10-24 22:46:29 +0500

    Updated getDashboard function and added getSeller

--- controllers/general.js
+++ controllers/general.js
@@ -1,32 +1,37 @@
-import User from "../models/User.js"
-import Staff from "../models/Staff.js"
-import Doctor from "../models/Doctor.js"
-
+import User from "../models/User.js";
+import Cattle from "../models/Cattle.js";
+import OnSaleCattle from "../models/OnSaleCattle.js";
+import Seller from "../models/Seller.js";
 
 export const getDashboardStats = async (req, res) => {
-    try {
-      const doctors = await Doctor.find({});
-      const staff = await Staff.find({});
-    
-      const totalDoctors = 1;
-      const totalStaff = 1;
-      const thisMonthStaffHired = 1;
-      const thisMonthDoctorsHired = 1;
-      const todayStaffHired = 0;
-      const todayDoctorsHired = 0;
-  
-      res.status(200).json({
-        totalStaff,
-        totalDoctors,
-        thisMonthStaffHired,
-        thisMonthDoctorsHired,
-        todayStaffHired,
-        todayDoctorsHired,
-        doctors,
-        staff
-      });
+  try {
+    const users = await User.find({});
+    const cattle = await Cattle.find({});
+    const onSaleCattle = await OnSaleCattle.find({});
 
-    } catch (error) {
-      res.status(404).json({ message: error.message });
-    }
-  };
+    const totalUsers = users.length;
+    const totalCattle = cattle.length;
+    const totalOnSaleCattle = onSaleCattle.length;
+    const totalIndividualSellers = 1;
+    const totalFarmSellers = 0;
+
+    res.status(200).json({
+      totalCattle,
+      totalFarmSellers,
+      totalIndividualSellers,
+      totalOnSaleCattle,
+      totalUsers,
+    });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+export const getSeller = async (req, res) => {
+  try {
+    const seller = await Seller.findById(req.params.id).populate("profile");
+
+    res.status(200).json(seller);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
