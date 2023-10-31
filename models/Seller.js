4451d790e589343147f88f2c92b54141047a4136
tahir_anjum <imtahiranjum@gmail.com>
2023-10-31 23:58:28 +0500

    added seller model

---
+++ models/Seller.js
@@ -0,0 +1,73 @@
+import mongoose, { Schema } from "mongoose";
+
+const SellerSchema = new mongoose.Schema(
+  {
+    profile: {
+      type: Schema.Types.ObjectId,
+      ref: "Profile"
+    },
+
+    name: {
+      type: String,
+      required: true,
+    },
+
+    display_name: {
+      type: String,
+    },
+
+    description: {
+      type: String,
+    },
+    
+    rating: {
+      type: Number,
+    },
+
+    contact_info: {
+      phone_number: {
+        type: Number,
+      },
+      location: {
+        latitude: {
+          type: String,
+        },
+        longitude: {
+          type: String,
+        },
+      },
+      address: {
+        type: String,
+      },
+    },
+
+    expired_boarding_services: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "ExpiredBoardingService",
+      },
+    ],
+    expired_cattle_on_sale: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "ExpiredOnSaleCattle",
+      },
+    ],
+    boarding_services: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "BoardingService",
+      },
+    ],
+    cattle_on_sale: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "OnSaleCattle",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const Seller = mongoose.model("Seller", SellerSchema);
+export default Seller;
