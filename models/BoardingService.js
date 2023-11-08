07283f399f7a9b742bb507af2b6a5347ba942730
Tahir Anjum <imtahiranjum@gmail.com>
2023-11-08 23:31:47 +0500

    Added New Schema

---
+++ models/BoardingService.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const BoardingServiceSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 100,
+    },
+
+    services: [
+      {
+        type: String,
+        min: 10,
+        max: 500,
+      },
+    ],
+
+    location: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 100,
+    },
+    details: {
+      type: String,
+      min: 10,
+      max: 500,
+    },
+  },
+  { timestamps: true }
+);
+
+const BoardingService = mongoose.model(
+  "BoardingService",
+  BoardingServiceSchema
+);
+export default BoardingService;
