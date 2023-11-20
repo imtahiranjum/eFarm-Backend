e3d3d43ebd9ea92deb37a9bbf41e49d2bf7930fc
Tahir Anjum <imtahiranjum@gmail.com>
2023-11-20 23:51:12 +0500

    Add createSeller route to user.js

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createUser, getUser, getUserByEmail, loginUser, logoutUser } from "../controllers/user.js";
+import { createSeller, createUser, getUser, getUserByEmail, loginUser, logoutUser } from "../controllers/user.js";
 
 const router = express.Router();
 
 router.post("/signup", createUser);
 router.post("/signin", loginUser);
+router.post("/createseller", createSeller);
 router.get("/id/:id", getUser);
 router.get("/email/:email", getUserByEmail);
 router.post("/logout", logoutUser);
