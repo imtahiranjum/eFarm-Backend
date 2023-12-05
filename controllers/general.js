1c531f52a47c87a5fef05f8ff71bcbc480d5b337
Tahir Anjum <imtahiranjum@gmail.com>
2023-12-05 20:18:51 +0500

    Add Question and Answer functionality

--- controllers/general.js
+++ controllers/general.js
@@ -2,6 +2,7 @@ import User from "../models/User.js";
 import Cattle from "../models/Cattle.js";
 import OnSaleCattle from "../models/OnSaleCattle.js";
 import Seller from "../models/Seller.js";
+import Question from "../models/Question.js";
 
 export const getDashboardStats = async (req, res) => {
   try {
@@ -31,6 +32,37 @@ export const getSeller = async (req, res) => {
     const seller = await Seller.findById(req.params.id).populate("profile");
 
     res.status(200).json(seller);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+export const addQuestion = async (req, res) => {
+  try {
+    const payload = req.body;
+    console.log(payload);
+    const question = new Question({
+      description: payload.description,
+      user: payload.user,
+      onsalecattle: payload.onsalecattle,
+    });
+    await question.save();
+    const onSaleCattle = await OnSaleCattle.findByIdAndUpdate(
+      { _id: payload.onsalecattle },
+      { $push: { questions: question._id } }
+    );
+    await onSaleCattle.save();
+    res.status(200).json(question);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+export const addAnswer = async (req, res) => {
+  try {
+    const payload = req.body;
+    const question = await Question.findByIdAndUpdate( { _id: payload.questionId } );
+    question.answer = payload.answer;
+    const savedQuestion = await question.save();
+    res.status(200).json(question);
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
