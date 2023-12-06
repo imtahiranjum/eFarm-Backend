2e02a3db178604fdc2ca47e4f05d3b65c3e09944
Tahir Anjum <imtahiranjum@gmail.com>
2023-12-06 20:46:20 +0500

    Refactor OnSaleCattleSchema in models/OnSaleCattle.js

--- models/OnSaleCattle.js
+++ models/OnSaleCattle.js
@@ -2,6 +2,43 @@ import mongoose, { Schema } from "mongoose";
 
 const OnSaleCattleSchema = new mongoose.Schema(
   {
+    name: {
+      type: String,
+      required: true,
+    },
+
+    gender: {
+      type: String,
+      required: true,
+    },
+
+    breed: {
+      type: String,
+      required: true,
+    },
+
+    color: {
+      type: String,
+      required: true,
+    },
+    weight: {
+      type: String,
+      required: true,
+    },
+
+    age: {
+      type: String,
+      required: true,
+    },
+
+    is_boarded: {
+      type: Boolean,
+    },
+
+    category: {
+      type: String,
+    },
+
     title: {
       type: String,
       required: true,
@@ -36,31 +73,18 @@ const OnSaleCattleSchema = new mongoose.Schema(
       },
     ],
 
+    seller_info: {
+      type: Schema.Types.ObjectId,
+      ref: "Seller",
+      required: true,
+    },
+
     questions: [
       {
         type: Schema.Types.ObjectId,
         ref: "Question",
       },
     ],
-
-    seller_info: {
-      type: Schema.Types.ObjectId,
-      ref: "Seller",
-      required: true,
-    },
-    
-    cattle_info: {
-      type: Schema.Types.ObjectId,
-      ref: "Cattle",
-      required: true,
-    },
-
-    // questions: [
-    //   {
-    //     type: Schema.Types.ObjectId,
-    //     ref: "Question",
-    //   },
-    // ],
   },
   { timestamps: true }
 );
