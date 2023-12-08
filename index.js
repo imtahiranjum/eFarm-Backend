0c49effa422eaab26377a3f8aec73385a24af38d
Tahir Anjum <imtahiranjum@gmail.com>
2023-12-08 08:40:43 +0500

    Refactor CORS methods in socket.io server

--- index.js
+++ index.js
@@ -44,25 +44,8 @@ const PORT = process.env.PORT || 9000;
 const io = new Server({
   cors: {
     origin: "http://localhost:3000",
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    methods: ["GET", "POST"],
   },
-});
-
-io.on("connection", (socket) => {
-  console.log(`User Connected: ${socket.id}`);
-
-  socket.on("join_room", (data) => {
-    socket.join(data);
-    console.log(`User with ID: ${socket.id} joined room: ${data}`);
-  });
-
-  socket.on("send_message", (data) => {
-    socket.to(data.room).emit("receive_message", data);
-  });
-
-  socket.on("disconnect", () => {
-    console.log("User Disconnected", socket.id);
-  });
 });
 
 mongoose.connect(process.env.MONGO_URL, {
